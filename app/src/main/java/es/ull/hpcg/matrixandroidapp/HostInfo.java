bddd19803305f3f34a43ea3a737c2e7088568383
Pavel Nichita <pnichita@ull.edu.es>
2019-03-13 15:17:04 +0000

    -Add class that implements Interface with device hostname/info

---
+++ app/src/main/java/es/ull/hpcg/matrixandroidapp/HostInfo.java
@@ -0,0 +1,23 @@
+package es.ull.hpcg.matrixandroidapp;
+
+import android.os.Build;
+
+import matrix.lib.IHostInfo;
+
+public class HostInfo implements IHostInfo {
+    @Override
+    public String getName() {
+        StringBuilder hostname = new StringBuilder();
+        hostname.append(Build.DEVICE)
+                .append("***")
+                .append(Build.SERIAL)
+                .append("***")
+                .append(Build.ID)
+                .append("***")
+                .append(Build.BOARD)
+                .append("***")
+                .append(Build.HARDWARE);
+
+        return hostname.toString();
+    }
+}
